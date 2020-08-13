61cbe2d68531ba7f7ed03730b6d0a229f445aa1b
Listeny <yanxiaona329@163.com>
2020-08-13 20:46:58 +0800

    后台首页面品质结构开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,128 @@
 <template>
-    <div>后台首页面</div>
+    <el-container>
+      <el-header>
+          <div class="logo-box">
+              <img src="../assets/img/head.png" alt="">
+              <span>电商后台管理系统</span>
+          </div>
+          <el-button type="info" @click="logout()">退出</el-button>
+      </el-header>
+      <el-container>
+        <el-aside :width="isshow? '65px':'200px'">
+            <div class="toggle-bar" @click="isshow=!isshow" :style="{width:isshow? '65px':'200px'}">|||</div>
+            <el-menu background-color="#333744" text-color="#ffff" active-text-color="#409eff" :unique-opened="true" :collapse-transition="false" :collapse="isshow">
+              <el-submenu
+              :index="item.id"
+              :style="{width:isshow? '65px':'200px'}"
+              v-for=" (item, k) in menuList "
+              :key="item.id"
+              >
+                <template slot="title">
+                  <i :class="'iconfont icon-'+iconList[k]"></i>
+                  <span slot="title">{{ item.authName }}</span>
+                </template>
+                <el-menu-item
+                :index="item.id+'-'+item2.id"
+                v-for=" item2 in item.children "
+                :key="item2.id">
+                <i class="el-icon-menu"></i>
+                <span slot="title">{{ item2.authName }}</span>
+              </el-menu-item>
+              </el-submenu>
+            </el-menu>
+        </el-aside>
+        <el-main>
+          <router-view></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
 </template>
 <script>
-export default {}
+export default {
+  // 生命周期函数
+  created () {
+    this.getMenuList()
+  },
+  methods: {
+    logout () {
+      this.$confirm('确定要退出系统么?', '退出', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      })
+        .then(() => {
+          // 删除token
+          window.sessionStorage.removeItem('token')
+          // 重定向到登录页面
+          this.$router.push('/login')
+        })
+        .catch(() => {
+        })
+    },
+    // (axios)获得左侧导航数据信息
+    async getMenuList () {
+      const { data: dt } = await this.$http.get('/menus')
+      console.log(dt)
+      if (dt.meta.status !== 200) {
+        return this.$message.error(dt.meta.msg)
+      }
+      // 把获得到的导航权限数据 赋予给menuList成员
+      this.menuList = dt.data
+    }
+  },
+  data () {
+    return {
+      // 图标系列
+      iconList: ['users', 'tijikongjian', 'shangpin', 'danju', 'baobiao'],
+      // 接收左侧菜单的数据成员
+      menuList: [],
+      // 左侧导航菜单默认展开
+      isshow: false
+    }
+  }
+}
+
 </script>
 <style lang="less" scoped>
+.el-container {
+    height: 100%;
+    .el-header {
+        background-color: #373d41;
+        display: flex;
+        align-items: center;
+        padding: 0;
+        padding-right: 20px;
+        justify-content: space-between;
+        .logo-box {
+            color: #ffff;
+            font-size: 22px;
+            display: flex;
+            align-items: center;
+            user-select: none;
+            img {
+                width: 50px;
+                height: 50px;
+                margin-right: 10px;
+            }
+        }
+    }
+    .el-aside {
+        background-color: #333744;
+        .toggle-bar {
+            width: 200px;
+            height: 25px;
+            line-height: 25px;
+            background-color: #4a5064;
+            text-align: center;
+            color: #ffff;
+            font-size: 12px;
+            letter-spacing: 0.1em;
+            user-select: none;
+            cursor: pointer;
+        }
+    }
+    .el-main {
+        background-color: #eaedf1;
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -12,6 +12,20 @@ import ElementUI from 'element-ui'
 Vue.use(ElementUI)
 // 给axios做配置
 axios.defaults.baseURL = 'http://127.0.0.1:11333/api/private/v1/'
+axios.interceptors.request.use(
+  function (config) {
+    // config: 对象，是axios的子级成员
+    // 可以对其进行配置，这个配置信息直接会作用给axios
+    // 获取token
+    var token = window.sessionStorage.getItem('token')
+    // 给axios的请求头中设置token信息，名称为Authorization
+    config.headers.Authorization = token
+    return config
+  },
+  function (error) {
+    return Promise.reject(error)
+  }
+)
 Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
